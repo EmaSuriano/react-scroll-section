99d9e7549185aac78b1b5ebd0ff7b82fce090f4e
EmaSuriano <emanuel.suriano@gmail.com>
2020-06-14 21:27:15 +0200

    feat: export context

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ScrollingProvider from './ScrollingProvider';
 import Section from './Section';
 import SectionLink from './SectionLink';
 import SectionLinks from './SectionLinks';
+import { ScrollContext } from './scrollContext';
 
-export { ScrollingProvider, Section, SectionLink, SectionLinks };
+export { ScrollingProvider, Section, SectionLink, SectionLinks, ScrollContext };
