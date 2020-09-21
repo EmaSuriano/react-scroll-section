bf761e0d5cf4d692bef34b62f090205fe1ee6996
EmaSuriano <emanuel.suriano@gmail.com>
2020-09-21 17:02:35 +0200

    fixing build result

--- nwb.config.js
+++ nwb.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   type: 'react-component',
   npm: {
-    esModules: true,
-    umd: {
-      global: 'react-scroll-section',
-      externals: {
-        react: 'React',
-      },
-    },
+    cjs: false,
+    esModules: false,
+    umd: false,
   },
   webpack: {
     config: (config) => {
