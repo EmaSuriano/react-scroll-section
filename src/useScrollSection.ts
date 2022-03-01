4bdd41e9da2fa479e304e2cbb835912fb9f05c9f
Селезнев Сергей <s.seleznev@mixit.ru>
2022-03-01 20:33:50 +0500

    #memo/click-handler: memoize click handler with useCallback in hooks

--- src/useScrollSection.ts
+++ src/useScrollSection.ts
@@ -1,23 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import { ScrollContext } from './context';
 
 export const useScrollSection = (id: string) => {
   const { scrollTo, selected: selectedSection } = useContext(ScrollContext);
-  const onClick = () => scrollTo(id);
+  const onClick = useCallback(() => scrollTo(id), [scrollTo, id]);
   const selected = selectedSection === id;
 
   return { onClick, selected };
 };
 
 export const useScrollSections = () => {
-  const { scrollTo, selected: selectedSection, refs, meta } = useContext(
-    ScrollContext,
-  );
+  const {
+    scrollTo,
+    selected: selectedSection,
+    refs,
+    meta,
+  } = useContext(ScrollContext);
+  const createOnClick = useCallback((id) => () => scrollTo(id), [scrollTo]);
 
   const sections = Object.keys(refs).map((id) => ({
     id,
     meta: meta[id],
-    onClick: () => scrollTo(id),
+    onClick: createOnClick(id),
     selected: selectedSection === id,
   }));
 
